55d1d67dfce3dd6464122ddf58bd1b0b8df45c3f
Aznyi <colbyjameslopez@gmail.com>
2026-02-04 16:45:03 -0600

    More SQL corrections: 1. The update 45 from my private repo is already applied to the character DB dump this is redundant. 2. New logon databases do not require the switch to encrypted passwords this is if you had existing accounts.. removing. 3. Updated 18_Items_Sockets.sql and 19_Items.sql for proper SQL structure to remediate warnings from HeidiSQL

--- Alpha/Core/sql/world_updates/18_Items_Sockets.sql
+++ Alpha/Core/sql/world_updates/18_Items_Sockets.sql
@@ -1,6 +1,6 @@
-ALTER TABLE items CHANGE socket_color_1 socketColor_1 tinyint(4) NOT NULL DEFAULT '0';
-ALTER TABLE items CHANGE unk201_3 socketContent_1 tinyint(4) NOT NULL DEFAULT '0';
-ALTER TABLE items CHANGE socket_color_2 socketColor_2 tinyint(4) NOT NULL DEFAULT '0';
-ALTER TABLE items CHANGE unk201_5 socketContent_2 tinyint(4) NOT NULL DEFAULT '0';
-ALTER TABLE items CHANGE socket_color_3 socketColor_3 tinyint(4) NOT NULL DEFAULT '0';
-ALTER TABLE items CHANGE unk201_7 socketContent_3 tinyint(4) NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE socket_color_1 socketColor_1 tinyint NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE unk201_3 socketContent_1 tinyint NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE socket_color_2 socketColor_2 tinyint NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE unk201_5 socketContent_2 tinyint NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE socket_color_3 socketColor_3 tinyint NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE unk201_7 socketContent_3 tinyint NOT NULL DEFAULT '0';
--- Alpha/Core/sql/world_updates/19_Items.sql
+++ Alpha/Core/sql/world_updates/19_Items.sql
@@ -1,5 +1,5 @@
-ALTER TABLE items CHANGE RequiredSkillSubRank `requiredspell` mediumint(8) unsigned NOT NULL DEFAULT '0';
-ALTER TABLE items CHANGE RequiredPlayerRank1 `requiredhonorrank` mediumint(8) unsigned NOT NULL DEFAULT '0';
-ALTER TABLE items CHANGE RequiredPlayerRank2 `RequiredCityRank` mediumint(8) unsigned NOT NULL DEFAULT '0';
-ALTER TABLE items CHANGE RequiredFaction `RequiredReputationFaction` smallint(5) unsigned NOT NULL DEFAULT '0';
-ALTER TABLE items CHANGE RequiredFactionStanding `RequiredReputationRank` smallint(5) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE RequiredSkillSubRank `requiredspell` mediumint unsigned NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE RequiredPlayerRank1 `requiredhonorrank` mediumint unsigned NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE RequiredPlayerRank2 `RequiredCityRank` mediumint unsigned NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE RequiredFaction `RequiredReputationFaction` smallint unsigned NOT NULL DEFAULT '0';
+ALTER TABLE items CHANGE RequiredFactionStanding `RequiredReputationRank` smallint unsigned NOT NULL DEFAULT '0';
